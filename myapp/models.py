babe2ee34eafa8a47da136a68939813a56bf29a9
Ian Andwati <andwatiian@gmail.com>
2024-02-19 13:54:40 +0300

    completed views

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=30, null=True, blank=True)
+    price = models.CharField(max_length=40, null=True, blank=True)
+    quantity = models.IntegerField(null=True, blank=True)
+
+    
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import viewsets
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+
+class GetMethod(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def list(self, request, *args, **kwargs):
+        data = list(Product.objects.all().values())
+        return Response(data, status=status.HTTP_200_OK)
+    
+    def retrieve(self, request, *args, **kwargs):
+        data = list(Product.objects.filter(id=kwargs['pk']).values())
+        return Response(data, status=status.HTTP_200_OK)
+    
+    def create(self, request, *args, **kwargs):
+        product_serializer_data = ProductSerializer(data=request.data)
+        if product_serializer_data.is_valid():
+            product_serializer_data.save()
+            status_code = status.HTTP_201_CREATED
+            return Response({'message': 'Product added successfully'}, status=status_code)
+        else:
+            status_code = status.HTTP_400_BAD_REQUEST
+            return Response({"message": "please fill the details"}, status=status_code)
+        
+
+    def destroy(self, request, *args, **kwargs):
+        product_data = Product.objects.filter(id=kwargs['pk'])
+        if product_data:
+            product_data.delete()
+            status_code = status.HTTP_201_CREATED
+            return Response({'message': 'Product deleted successfully'}, status=status_code)
+        else:
+            status_code = status.HTTP_400_BAD_REQUEST
+            return Response({'message': 'Product not found'}, status=status_code)
+
+    def update(self, request, *args, **kwargs):
+        product_details = Product.objects.get(id=kwargs['pk'])
+        product_serializer_data = ProductSerializer(product_details, data=request.data, partial=True)
+        if product_serializer_data.is_valid():
+            product_serializer_data.save()
+            status_code = status.HTTP_201_CREATED
+            return Response({'message': 'Product updated successfully'}, status=status_code)
+        else:
+            status_code = status.HTTP_400_BAD_REQUEST
+            return Response({'message': 'Product not found'}, status=status_code)
