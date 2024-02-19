a62df2a63754652fb70426478679cc22be391181
Ian Andwati <andwatiian@gmail.com>
2024-02-19 13:38:37 +0300

    initial app setup

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myapp.apps.MyappConfig',
+    'rest_framework_swagger',
+    'rest_framework',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
